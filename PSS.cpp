7dee22a10fc7b75610513c590f6afd76d9bc5272
szunovy <szunovy@wp.pl>
2023-04-25 21:17:08 +0200

    part of reading from file

--- PSS.cpp
+++ PSS.cpp
@@ -1,4 +1,9 @@
 // PSS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//to avoid warnings with strtok 
+//TODO change later to strtok_s
+#define _CRT_SECURE_NO_WARNINGS
+
+
 #include "Siso.h"
 #include "Arx.h"
 #include "Regulator.h"
@@ -7,8 +12,12 @@
 #include <iomanip>
 #include <stdlib.h>
 #include <time.h>
+#include <fstream>
+#include<string>
+#include <cstring>
 using namespace std;
 
+const int INPUT_PARAMS_LINE_LENGTH = 30;
 
 int main()
 {
@@ -33,16 +42,56 @@ int main()
 	double output = 0;
 	int simTime = 0;
 	int stopTime = 20;
-	while (simTime<stopTime)
+	//while (simTime<stopTime)
 	//while(1)
+	//{
+	//	//x = rand() % 5;
+	//	x = 3;
+	//	output = modelArx.simulate(x);
+	//	cout << output;
+	//	cout << endl;
+	//	simTime++;
+	//}
+	ifstream input_file("data\\input_parameters.txt");
+	//input_file.open;
+	string parameters_file_line;
+	int lineno = 0;
+	//getting params from txt file line by line until, each line is a new model
+	//params sets are separated by semicolon, and each parameter is separated by whitespace
+	//e.g 
+	//a1 a2 a3 ...;b0 b1 b2 b3 ...	    //[model no.1]
+	//a1 a2 a3...;b0 b1 b2 b3...		//[model no.2]
+	//... etc.
+	while(getline(input_file, parameters_file_line)) 
 	{
-		//x = rand() % 5;
-		x = 3;
-		output = modelArx.simulate(x);
-		cout << output;
-		cout << endl;
-		simTime++;
+		lineno++;  //counting lines to choose model
+		char* line_array = new char[parameters_file_line.length() + 1];
+		strcpy(line_array, parameters_file_line.c_str());
+		//cout << parameters_file_line<<endl;
+
+		//obtaining parameters sets (Arx: A and B) from line by semicolon separator
+		char* p = strtok(line_array, ";");
+		while (p != NULL)
+		{
+			cout <<"sizeof line "<< sizeof( p) << endl;
+			
+			//obtaining each parameter
+			char* parameter_from_line = strtok(p, " ");
+			cout << "parametry z jednej lini" << endl;
+			while (parameter_from_line != NULL)
+			{
+				//string final_parameter(parameter_from_line);
+				cout << stod(parameter_from_line) << endl;
+				parameter_from_line = strtok(NULL, " ");
+			}
+
+			p = strtok(NULL, ";");
+		}
+		delete[] line_array;
 	}
+
+	input_file.close();
+
 
 
 	return 0;
